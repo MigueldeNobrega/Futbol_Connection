b25fe279038abe5814be6da2a9ee53f7fffe9eca
MigueldeNobrega <miguel.eenobrega@gmail.com>
2024-10-26 19:58:09 +0200

    Funciones entre Main Login y Registro

--- app/src/main/java/com/example/futbol_connection/MainActivity.java
+++ app/src/main/java/com/example/futbol_connection/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.futbol_connection;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +27,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        Button changeToLogin = findViewById(R.id.b_mainToLogin);
+        changeToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Inicia Sesión", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this,  LoginActivity.class));
+            }
+        });
+
+
+        TextView changeToRegistro = findViewById((R.id.t_mainToRegistro));
+
+        changeToRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Crea una Cuenta", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this, RegistroActivity.class));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
