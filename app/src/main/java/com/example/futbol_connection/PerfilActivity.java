317851b03fafaa527326f84e80ff779919129bac
MigueldeNobrega <miguel.eenobrega@gmail.com>
2024-11-17 16:02:42 +0100

    PerfilIniciadoDatos

--- app/src/main/java/com/example/futbol_connection/PerfilActivity.java
+++ app/src/main/java/com/example/futbol_connection/PerfilActivity.java
@@ -1,10 +1,6 @@
 package com.example.futbol_connection;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuInicioActivity extends AppCompatActivity {
+public class PerfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu_inicio);
+        setContentView(R.layout.activity_perfil);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        TextView changeToPerfil = findViewById((R.id.t_menuToPerfil));
-
-        changeToPerfil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MenuInicioActivity.this, "Mi Perfil", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(MenuInicioActivity.this, PerfilActivity.class));
-            }
-        });
-
-
     }
 }
